9fc4641ef97bec22894cdf13570c282e083fa6dc
mrjvs <jellevs@gmail.com>
2023-11-15 20:09:53 +0100

    try fixing the hooks, due to incompetent rerendering logic its causing infinite looks

--- src/react/hooks/useState.ts
+++ src/react/hooks/useState.ts
@@ -1,22 +1,24 @@
 import { initializeHook, taintNode } from '@/react/hooks';
 
+type UseStateSetter<T> = T | ((s: T) => T);
+
 interface UseStateHook<T> {
   d?: T;
+  set?: (val: UseStateSetter<T>) => void;
 }
-
-type UseStateSetter<T> = T | ((s: T) => T);
 
 export function useState<T>(initial: T) {
   const hook = initializeHook<UseStateHook<T>>();
-  if (hook.initial) hook.data.d = initial;
-
-  function set(val: UseStateSetter<T>) {
-    let newValue: any = val;
-    if (typeof val === 'function') newValue = (val as any)(hook.data.d);
-    if (newValue === hook.data.d) return; // if same value, ignore setter
-    hook.data.d = newValue;
-    taintNode(hook.node);
+  if (hook.initial) {
+    hook.data.d = initial;
+    hook.data.set = (val: UseStateSetter<T>) => {
+      let newValue: any = val;
+      if (typeof val === 'function') newValue = (val as any)(hook.data.d);
+      if (newValue === hook.data.d) return; // if same value, ignore setter
+      hook.data.d = newValue;
+      taintNode(hook.node);
+    };
   }
 
-  return [hook.data.d as T, set] as const;
+  return [hook.data.d as T, hook.data.set as (val: UseStateSetter<T>) => void] as const;
 }
--- test/main.tsx
+++ test/main.tsx
@@ -1,22 +1,43 @@
-import { render, useState } from "@/index"
+import { render, useEffect, useState } from '@/index';
 
 function Button(props: { onClick: () => void }) {
-  return <button onClick={props.onClick}>click me</button>
+  return <button onClick={props.onClick}>click me</button>;
+}
+
+function Incrementer() {
+  const [num, setNum] = useState(0);
+  useEffect(() => {
+    const a = setInterval(() => {
+      setNum((n) => n + 1);
+    }, 100);
+    return () => {
+      clearInterval(a);
+    };
+  }, [setNum]);
+
+  return (
+    <div>
+      <p>increment: {num}</p>
+    </div>
+  );
 }
 
 function App() {
   const [num, setNum] = useState(0);
 
-  return <div>
-    <p>number: {num}</p>
-    <Button onClick={() => setNum(n => n+1)}/>
-  </div>
+  return (
+    <div>
+      <p>number: {num}</p>
+      <Button onClick={() => setNum((n) => n + 1)} />
+      <Incrementer />
+    </div>
+  );
 }
 
 const dom = <App />;
-render(dom, document.getElementById("app"));
+render(dom, document.getElementById('app'));
 
-const log = document.getElementById("log");
+const log = document.getElementById('log');
 setInterval(() => {
   log.innerText = JSON.stringify(dom, null, 2);
 }, 1);
